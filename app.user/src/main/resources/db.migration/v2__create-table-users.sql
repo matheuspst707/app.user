6ddb5d9fb9121ef576c5752c518a81c32e28308b
matheusol1veir4 <matheus.colvr@gmail.com>
2025-01-15 12:30:46 -0300

    banco funcional

--- app.user/src/main/resources/db.migration/v2__create-table-users.sql
+++
@@ -1,8 +0,0 @@
-CREATE TABLE users (
-    id VARCHAR(255) PRIMARY KEY NOT NULL UNIQUE,
-    login VARCHAR(255) NOT NULL UNIQUE,
-    password VARCHAR(255) NOT NULL,
-    role VARCHAR(50) NOT NULL);
-    
-    INSERT INTO users (login, password, role)  
-VALUES ('usuario_admin', '$2a$12$swLZnlsz1Bo9IgHva8RYxeHinzZGdQouVZ2hmgq8LnVvOezFYw1yi', 'ROLE_ADMIN');
